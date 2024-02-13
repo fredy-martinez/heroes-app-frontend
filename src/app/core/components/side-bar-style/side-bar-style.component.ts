fd978c6196c79537941eee910647f2d29aca40c8
Fredy Amilcar Martinez Girón <fredy.martinez@senprende.hn>
2024-02-13 16:31:25 -0600

    Menu de sidebar

--- src/app/core/components/side-bar-style/side-bar-style.component.ts
+++ src/app/core/components/side-bar-style/side-bar-style.component.ts
@@ -1,19 +1,33 @@
 import { Component } from '@angular/core';
+import { MenuItem } from 'primeng/api';
 import { ButtonModule } from 'primeng/button';
 import { SidebarModule } from 'primeng/sidebar';
+import { MenuModule } from 'primeng/menu';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-side-bar-style',
   standalone: true,
-  imports: [SidebarModule, ButtonModule],
+  imports: [CommonModule,  SidebarModule, ButtonModule, MenuModule],
   templateUrl: './side-bar-style.component.html',
   styleUrl: './side-bar-style.component.scss'
 })
 export class SideBarStyleComponent {
 
   sidebarVisible: boolean = false;
-
+  items: MenuItem[] | undefined;
+  
   constructor(){}
+
+  ngOnInit() {
+    this.items = [{
+        items: [
+            {label: 'New', icon: 'pi pi-plus', url: 'https://primeng.org'},
+            {label: 'Open', icon: 'pi pi-download', routerLink: ['/menu']},
+            {label: 'Recent Files', icon: 'pi pi-download', routerLink: ['/menu']}
+        ]
+    }];
+}
 
   public sidebarItems = [
     {
